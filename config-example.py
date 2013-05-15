d8c5fcba829736fac4ef340d4aa4c78920e5a133
Benson Kalahar <bensonk@google.com>
2013-05-14 22:47:57 -0700

    Cleans up and comments config-example.py

--- config-example.py
+++ config-example.py
@@ -1,6 +1,17 @@
-BUCKET = 'delete-test'
+# Your bucket to delete things from
+BUCKET = 'ADDME'
+
+# How many processes to fork
 PROCESS_COUNT = 120
+
+# Maximum number of objects per bulk request
 MAX_JOB_SIZE = 10000
+
+# Your developer key from the APIs tab of https://code.google.com/apis/console
 DEVKEY = 'ADDME'
+
+# Your client ID from same
 CLIENT_ID = 'ADDME'
+
+# Your client secret from same
 CLIENT_SECRET = 'ADDME'
